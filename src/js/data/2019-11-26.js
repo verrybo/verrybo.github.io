bd4dab35f5c8ae9a644905030ba35e4908ab8224
verrybo <77595036+verrybo@users.noreply.github.com>
2021-01-18 14:47:52 +0200

    Update 2019-11-26.js

--- src/js/data/2019-11-26.js
+++ src/js/data/2019-11-26.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2019-11-26"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2021-01-18"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
@@ -16,11 +16,11 @@ dataSet[dataSetVersion].options = [
       { name: "0083: Stardust Memory", key: "83" },
       { name: "Mobile Suit Gundam F91", key: "F91" },
       { name: "Mobile Suit Victory Gundam", key: "V" },
-      { name: "Mobile Suit Gundam Wing", key: "Wing" },
+      { name: "Mobile Suit Gundam Wing", key: "WING" },
       { name: "The 08th MS Team", key: "08" },
       { name: "After War Gundam X", key: "X" },
       { name: "Gundam Wing Endless Waltz", key: "EW" },
-      { name: "Turn A Gundam", key: "Turn" },
+      { name: "Turn A Gundam", key: "TURN" },
       { name: "Gundam Evolve", key: "EVO" },
       { name: "Mobile Suit Gundam SEED", key: "SEED" },
       { name: "MS IGLOO", key: "IG" },
@@ -38,7 +38,7 @@ dataSet[dataSetVersion].options = [
       { name: "Mobile Suit Gundam Iron-Blooded Orphans", key: "IBO" },
       { name: "Mobile Suit Gundam Thunderbolt", key: "TB" },
       { name: "Gundam Build Divers", key: "BD" },
-      { name: "Mobile Suit Gundam Narrative", key: "NAR" },
+      { name: "Mobile Suit Gundam Narrative", key: "NT" },
       { name: "Gundam Build Divers Re:RISE", key: "RISE" }
     ]
   }
@@ -47,18 +47,226 @@ dataSet[dataSetVersion].options = [
 dataSet[dataSetVersion].characterData = [
   {
     name: "Mobile Suit Gundam",
-    img: "c5DqpgX.png",
+    img: "dwWwyCC.jpg",
     opts: {
       series: [ "79" ]
     }
   },
   {
-    name: "Hisoutensoku",
-    img: "P4JZ2it.png",
+    name: "Mobile Suit Zeta Gundam",
+    img: "3W8ozNR.jpg",
     opts: {
-      series: ["soku"],
-      stage: [],
-      notgirl: true 
+      series: [ "Z" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam ZZ",
+    img: "9WnXIWb.jpg",
+    opts: {
+      series: [ "ZZ" ]
+    }
+  },
+  {
+    name: "Char's Counterattack",
+    img: "VrqfhhV.jpg",
+    opts: {
+      series: [ "CCA" ]
+    }
+  },
+  {
+    name: "0080: War in the Pocket",
+    img: "2RxbZBZ.jpg",
+    opts: {
+      series: [ "80" ]
+    }
+  },
+  {
+    name: "0083: Stardust Memory",
+    img: "kSuhEnc.jpg",
+    opts: {
+      series: [ "83" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam F91",
+    img: "rN1NjZn.jpg",
+    opts: {
+      series: [ "F91" ]
+    }
+  },
+  {
+    name: "Mobile Suit Victory Gundam",
+    img: "f5i6uwG.jpg",
+    opts: {
+      series: [ "V" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam Wing",
+    img: "wx9ntq7.jpg",
+    opts: {
+      series: [ "WING" ]
+    }
+  },
+  {
+    name: "The 08th MS Team",
+    img: "rk0wbdx.jpg",
+    opts: {
+      series: [ "08" ]
+    }
+  },
+  {
+    name: "After War Gundam X",
+    img: "pBoMFuM.jpg",
+    opts: {
+      series: [ "X" ]
+    }
+  },
+  {
+    name: "Gundam Wing Endless Waltz",
+    img: "6JbFihB.jpg",
+    opts: {
+      series: [ "EW" ]
+    }
+  },
+  {
+    name: "Turn A Gundam",
+    img: "3rgmWcq.jpg",
+    opts: {
+      series: [ "TURN" ]
+    }
+  },
+  {
+    name: "Gundam Evolve",
+    img: "R1SEQth.jpg",
+    opts: {
+      series: [ "EVO" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam SEED",
+    img: "e3Yd2oD.jpg",
+    opts: {
+      series: [ "SEED" ]
+    }
+  },
+  {
+    name: "MS IGLOO (HOYW & A79)",
+    img: "lK8XLue.jpg",
+    opts: {
+      series: [ "IG" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam SEED Destiny",
+    img: "u6oyJ3R.jpg",
+    opts: {
+      series: [ "DEST" ]
+    }
+  },
+  {
+    name: "SEED C.E. 73: Stargazer",
+    img: "eXemVNh.jpg",
+    opts: {
+      series: [ "SG" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam 00",
+    img: "lkJ2LdF.jpg",
+    opts: {
+      series: [ "00" ]
+    }
+  },
+  {
+    name: "MS IGLOO 2: The Gravity Front",
+    img: "xyKP5ZF.jpg",
+    opts: {
+      series: [ "GF" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam Unicorn",
+    img: "mdym8lK.jpg",
+    opts: {
+      series: [ "UC" ]
+    }
+  },
+  {
+    name: "00: A Wakening of the Trailblazer",
+    img: "mO8r0w3.jpg",
+    opts: {
+      series: [ "AW" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam AGE",
+    img: "EUkWwkF.jpg",
+    opts: {
+      series: [ "AGE" ]
+    }
+  },
+  {
+    name: "Gundam Build Fighters",
+    img: "Za5m6Wd.jpg",
+    opts: {
+      series: [ "BF" ]
+    }
+  },
+  {
+    name: "Gundam Reconguista in G",
+    img: "UVDOJTA.jpg",
+    opts: {
+      series: [ "RECO" ]
+    }
+  },
+  {
+    name: "Gundam Build Fighters Try",
+    img: "FUKmxDL.jpg",
+    opts: {
+      series: [ "TRY" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam The Origin",
+    img: "CC19Fbs.jpg",
+    opts: {
+      series: [ "ORI" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam Iron-Blooded Orphans",
+    img: "RpoeLXb.png",
+    opts: {
+      series: [ "IBO" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam Thunderbolt",
+    img: "AJ0BydE.jpg",
+    opts: {
+      series: [ "TB" ]
+    }
+  },
+  {
+    name: "Gundam Build Divers",
+    img: "CtY0Txk.jpgg",
+    opts: {
+      series: [ "BD" ]
+    }
+  },
+  {
+    name: "Mobile Suit Gundam Narrative",
+    img: "uYykZL9.jpg",
+    opts: {
+      series: [ "NT" ]
+    }
+  },
+  {
+    name: "Gundam Build Divers",
+    img: "nGiDGCT.jpg",
+    opts: {
+      series: [ "RISE" ]
     }
   }
 ];
