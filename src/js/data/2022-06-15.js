ce18c0f2e538e678bd8fc110f29f1c231ff41a55
verrybo <77595036+verrybo@users.noreply.github.com>
2022-06-15 21:27:18 +0300

    Create 2022-06-15.js

---
+++ src/js/data/2022-06-15.js
@@ -0,0 +1,288 @@
+dataSetVersion = "2022-06-15"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Series Entry",
+    key: "series",
+    tooltip: "Check this to uncheck shows you haven't seen.",
+    checked: false,
+    sub: [
+      { name: "Mobile Suit Gundam", key: "79" },
+      { name: "Zeta Gundam", key: "Z" },
+      { name: "Gundam ZZ", key: "ZZ" },
+      { name: "Char's Counterattack", key: "CCA" },
+      { name: "0080: War in the Pocket", key: "80" },
+      { name: "0083: Stardust Memory", key: "83" },
+      { name: "Gundam F91", key: "F91" },
+      { name: "Victory Gundam", key: "V" },
+      { name: "Mobile Fighter G Gundam", key: "G" },
+      { name: "Gundam Wing", key: "WING" },
+      { name: "The 08th MS Team", key: "08" },
+      { name: "After War Gundam X", key: "X" },
+      { name: "Gundam Wing Endless Waltz", key: "EW" },
+      { name: "Turn A Gundam", key: "TURN" },
+      { name: "Gundam Evolve", key: "EVO" },
+      { name: "Gundam SEED", key: "SEED" },
+      { name: "MS IGLOO", key: "IG" },
+      { name: "Gundam SEED Destiny", key: "DEST" },
+      { name: "SEED C.E. 73: Stargazer", key: "SG" },
+      { name: "Gundam 00", key: "00" },
+      { name: "MS IGLOO 2: The Gravity Front", key: "GF" },
+      { name: "Gundam Unicorn", key: "UC" },
+      { name: "Gundam 00: A Wakening of the Trailblazer", key: "AW" },
+      { name: "Gundam AGE", key: "AGE" },
+      { name: "Gundam Build Fighters", key: "BF" },
+      { name: "Gundam Reconguista in G", key: "RECO" },
+      { name: "Gundam Build Fighters Try", key: "TRY" },
+      { name: "Gundam The Origin", key: "ORI" },
+      { name: "Gundam Iron-Blooded Orphans", key: "IBO" },
+      { name: "Gundam Thunderbolt", key: "TB" },
+      { name: "Gundam Build Divers", key: "BD" },
+      { name: "Gundam Narrative", key: "NT" },
+      { name: "Gundam Build Divers Re:RISE", key: "RISE" }
+      { name: "Gundam Hathaway", key: "HATH" }
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Mobile Suit Gundam",
+    img: "dwWwyCC.jpg",
+    opts: {
+      series: [ "79" ]
+    }
+  },
+  {
+    name: "Zeta Gundam",
+    img: "3W8ozNR.jpg",
+    opts: {
+      series: [ "Z" ]
+    }
+  },
+  {
+    name: "Gundam ZZ",
+    img: "9WnXIWb.jpg",
+    opts: {
+      series: [ "ZZ" ]
+    }
+  },
+  {
+    name: "Char's Counterattack",
+    img: "VrqfhhV.jpg",
+    opts: {
+      series: [ "CCA" ]
+    }
+  },
+  {
+    name: "0080: War in the Pocket",
+    img: "2RxbZBZ.jpg",
+    opts: {
+      series: [ "80" ]
+    }
+  },
+  {
+    name: "0083: Stardust Memory",
+    img: "kSuhEnc.jpg",
+    opts: {
+      series: [ "83" ]
+    }
+  },
+  {
+    name: "Gundam F91",
+    img: "rN1NjZn.jpg",
+    opts: {
+      series: [ "F91" ]
+    }
+  },
+  {
+    name: "Victory Gundam",
+    img: "f5i6uwG.jpg",
+    opts: {
+      series: [ "V" ]
+    }
+  },
+  {
+    name: "Mobile Fighter G Gundam",
+    img: "Umo8jfF.jpg",
+    opts: {
+      series: [ "G" ]
+    }
+  },
+  {
+    name: "Gundam Wing",
+    img: "wx9ntq7.jpg",
+    opts: {
+      series: [ "WING" ]
+    }
+  },
+  {
+    name: "The 08th MS Team",
+    img: "rk0wbdx.jpg",
+    opts: {
+      series: [ "08" ]
+    }
+  },
+  {
+    name: "After War Gundam X",
+    img: "pBoMFuM.jpg",
+    opts: {
+      series: [ "X" ]
+    }
+  },
+  {
+    name: "Gundam Wing Endless Waltz",
+    img: "6JbFihB.jpg",
+    opts: {
+      series: [ "EW" ]
+    }
+  },
+  {
+    name: "Turn A Gundam",
+    img: "3rgmWcq.jpg",
+    opts: {
+      series: [ "TURN" ]
+    }
+  },
+  {
+    name: "Gundam Evolve",
+    img: "R1SEQth.jpg",
+    opts: {
+      series: [ "EVO" ]
+    }
+  },
+  {
+    name: "Gundam SEED",
+    img: "e3Yd2oD.jpg",
+    opts: {
+      series: [ "SEED" ]
+    }
+  },
+  {
+    name: "MS IGLOO (HOYW & A79)",
+    img: "lK8XLue.jpg",
+    opts: {
+      series: [ "IG" ]
+    }
+  },
+  {
+    name: "Gundam SEED Destiny",
+    img: "u6oyJ3R.jpg",
+    opts: {
+      series: [ "DEST" ]
+    }
+  },
+  {
+    name: "SEED C.E. 73: Stargazer",
+    img: "eXemVNh.jpg",
+    opts: {
+      series: [ "SG" ]
+    }
+  },
+  {
+    name: "Gundam 00",
+    img: "lkJ2LdF.jpg",
+    opts: {
+      series: [ "00" ]
+    }
+  },
+  {
+    name: "MS IGLOO 2: The Gravity Front",
+    img: "xyKP5ZF.jpg",
+    opts: {
+      series: [ "GF" ]
+    }
+  },
+  {
+    name: " Gundam Unicorn",
+    img: "mdym8lK.jpg",
+    opts: {
+      series: [ "UC" ]
+    }
+  },
+  {
+    name: "00: A Wakening of the Trailblazer",
+    img: "mO8r0w3.jpg",
+    opts: {
+      series: [ "AW" ]
+    }
+  },
+  {
+    name: "Gundam AGE",
+    img: "EUkWwkF.jpg",
+    opts: {
+      series: [ "AGE" ]
+    }
+  },
+  {
+    name: "Gundam Build Fighters",
+    img: "Za5m6Wd.jpg",
+    opts: {
+      series: [ "BF" ]
+    }
+  },
+  {
+    name: "Gundam Reconguista in G",
+    img: "UVDOJTA.jpg",
+    opts: {
+      series: [ "RECO" ]
+    }
+  },
+  {
+    name: "Gundam Build Fighters Try",
+    img: "FUKmxDL.jpg",
+    opts: {
+      series: [ "TRY" ]
+    }
+  },
+  {
+    name: "Gundam The Origin",
+    img: "CC19Fbs.jpg",
+    opts: {
+      series: [ "ORI" ]
+    }
+  },
+  {
+    name: "Gundam Iron-Blooded Orphans",
+    img: "RpoeLXb.png",
+    opts: {
+      series: [ "IBO" ]
+    }
+  },
+  {
+    name: "Gundam Thunderbolt",
+    img: "AJ0BydE.jpg",
+    opts: {
+      series: [ "TB" ]
+    }
+  },
+  {
+    name: "Gundam Build Divers",
+    img: "CtY0Txk.jpg",
+    opts: {
+      series: [ "BD" ]
+    }
+  },
+  {
+    name: "Gundam Narrative",
+    img: "uYykZL9.jpg",
+    opts: {
+      series: [ "NT" ]
+    }
+  },
+  {
+    name: "Gundam Build Divers Re:RISE",
+    img: "nGiDGCT.jpg",
+    opts: {
+      series: [ "RISE" ]
+    }
+  },
+  {
+    name: "Gundam Hathaway",
+    img: "F9OTwy2.png",
+    opts: {
+      series: [ "HATH" ]
+    }
+  }
+];
