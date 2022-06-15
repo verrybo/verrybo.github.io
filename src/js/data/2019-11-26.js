7947783c80c2a4a3a670c13ea146688822389d8b
verrybo <77595036+verrybo@users.noreply.github.com>
2022-06-15 21:17:48 +0300

    Update 2019-11-26.js
    Adding Hathaway, mild edits also

--- src/js/data/2019-11-26.js
+++ src/js/data/2019-11-26.js
@@ -1,4 +1,4 @@
-dataSetVersion = "2021-01-18"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSetVersion = "2022-06-15"; // Change this when creating a new data set version. YYYY-MM-DD format.
 dataSet[dataSetVersion] = {};
 
 dataSet[dataSetVersion].options = [
@@ -9,38 +9,39 @@ dataSet[dataSetVersion].options = [
     checked: false,
     sub: [
       { name: "Mobile Suit Gundam", key: "79" },
-      { name: "Mobile Suit Zeta Gundam", key: "Z" },
-      { name: "Mobile Suit Gundam ZZ", key: "ZZ" },
+      { name: "Zeta Gundam", key: "Z" },
+      { name: "Gundam ZZ", key: "ZZ" },
       { name: "Char's Counterattack", key: "CCA" },
       { name: "0080: War in the Pocket", key: "80" },
       { name: "0083: Stardust Memory", key: "83" },
-      { name: "Mobile Suit Gundam F91", key: "F91" },
-      { name: "Mobile Suit Victory Gundam", key: "V" },
+      { name: "Gundam F91", key: "F91" },
+      { name: "Victory Gundam", key: "V" },
       { name: "Mobile Fighter G Gundam", key: "G" },
-      { name: "Mobile Suit Gundam Wing", key: "WING" },
+      { name: "Gundam Wing", key: "WING" },
       { name: "The 08th MS Team", key: "08" },
       { name: "After War Gundam X", key: "X" },
       { name: "Gundam Wing Endless Waltz", key: "EW" },
       { name: "Turn A Gundam", key: "TURN" },
       { name: "Gundam Evolve", key: "EVO" },
-      { name: "Mobile Suit Gundam SEED", key: "SEED" },
+      { name: "Gundam SEED", key: "SEED" },
       { name: "MS IGLOO", key: "IG" },
-      { name: "Mobile Suit Gundam SEED Destiny", key: "DEST" },
+      { name: "Gundam SEED Destiny", key: "DEST" },
       { name: "SEED C.E. 73: Stargazer", key: "SG" },
-      { name: "Mobile Suit Gundam 00", key: "00" },
+      { name: "Gundam 00", key: "00" },
       { name: "MS IGLOO 2: The Gravity Front", key: "GF" },
-      { name: "Mobile Suit Gundam Unicorn", key: "UC" },
+      { name: "Gundam Unicorn", key: "UC" },
       { name: "Gundam 00: A Wakening of the Trailblazer", key: "AW" },
-      { name: "Mobile Suit Gundam AGE", key: "AGE" },
+      { name: "Gundam AGE", key: "AGE" },
       { name: "Gundam Build Fighters", key: "BF" },
       { name: "Gundam Reconguista in G", key: "RECO" },
       { name: "Gundam Build Fighters Try", key: "TRY" },
-      { name: "Mobile Suit Gundam The Origin", key: "ORI" },
-      { name: "Mobile Suit Gundam Iron-Blooded Orphans", key: "IBO" },
-      { name: "Mobile Suit Gundam Thunderbolt", key: "TB" },
+      { name: "Gundam The Origin", key: "ORI" },
+      { name: "Gundam Iron-Blooded Orphans", key: "IBO" },
+      { name: "Gundam Thunderbolt", key: "TB" },
       { name: "Gundam Build Divers", key: "BD" },
-      { name: "Mobile Suit Gundam Narrative", key: "NT" },
+      { name: "Gundam Narrative", key: "NT" },
       { name: "Gundam Build Divers Re:RISE", key: "RISE" }
+      { name: "Gundam Hathaway", key: "HATH" }
     ]
   }
 ];
@@ -54,14 +55,14 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Zeta Gundam",
+    name: "Zeta Gundam",
     img: "3W8ozNR.jpg",
     opts: {
       series: [ "Z" ]
     }
   },
   {
-    name: "Mobile Suit Gundam ZZ",
+    name: "Gundam ZZ",
     img: "9WnXIWb.jpg",
     opts: {
       series: [ "ZZ" ]
@@ -89,14 +90,14 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam F91",
+    name: "Gundam F91",
     img: "rN1NjZn.jpg",
     opts: {
       series: [ "F91" ]
     }
   },
   {
-    name: "Mobile Suit Victory Gundam",
+    name: "Victory Gundam",
     img: "f5i6uwG.jpg",
     opts: {
       series: [ "V" ]
@@ -110,7 +111,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam Wing",
+    name: "Gundam Wing",
     img: "wx9ntq7.jpg",
     opts: {
       series: [ "WING" ]
@@ -152,7 +153,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam SEED",
+    name: "Gundam SEED",
     img: "e3Yd2oD.jpg",
     opts: {
       series: [ "SEED" ]
@@ -166,7 +167,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam SEED Destiny",
+    name: "Gundam SEED Destiny",
     img: "u6oyJ3R.jpg",
     opts: {
       series: [ "DEST" ]
@@ -180,7 +181,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam 00",
+    name: "Gundam 00",
     img: "lkJ2LdF.jpg",
     opts: {
       series: [ "00" ]
@@ -194,7 +195,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam Unicorn",
+    name: " Gundam Unicorn",
     img: "mdym8lK.jpg",
     opts: {
       series: [ "UC" ]
@@ -208,7 +209,7 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam AGE",
+    name: "Gundam AGE",
     img: "EUkWwkF.jpg",
     opts: {
       series: [ "AGE" ]
@@ -236,21 +237,21 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
-    name: "Mobile Suit Gundam The Origin",
+    name: "Gundam The Origin",
     img: "CC19Fbs.jpg",
     opts: {
       series: [ "ORI" ]
     }
   },
   {
-    name: "Mobile Suit Gundam Iron-Blooded Orphans",
+    name: "Gundam Iron-Blooded Orphans",
     img: "RpoeLXb.png",
     opts: {
       series: [ "IBO" ]
     }
   },
   {
-    name: "Mobile Suit Gundam Thunderbolt",
+    name: "Gundam Thunderbolt",
     img: "AJ0BydE.jpg",
     opts: {
       series: [ "TB" ]
@@ -258,13 +259,13 @@ dataSet[dataSetVersion].characterData = [
   },
   {
     name: "Gundam Build Divers",
-    img: "CtY0Txk.jpgg",
+    img: "CtY0Txk.jpg",
     opts: {
       series: [ "BD" ]
     }
   },
   {
-    name: "Mobile Suit Gundam Narrative",
+    name: "Gundam Narrative",
     img: "uYykZL9.jpg",
     opts: {
       series: [ "NT" ]
@@ -275,6 +276,13 @@ dataSet[dataSetVersion].characterData = [
     img: "nGiDGCT.jpg",
     opts: {
       series: [ "RISE" ]
+    }
+  },
+  {
+    name: "Gundam Hathaway",
+    img: "F9OTwy2.png",
+    opts: {
+      series: [ "HATH" ]
     }
   }
 ];
